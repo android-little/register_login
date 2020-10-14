bd3c81ca6b21c8c5db03b0e212c037662a2bc6a3
Kandrzez <wy2967105573@163.com>
2020-10-14 09:15:56 +0800

    zhenghe1

--- app/src/main/java/com/example/register_loginapp/MainActivity.java
+++ app/src/main/java/com/example/register_loginapp/MainActivity.java
@@ -1,15 +1,75 @@
 package com.example.register_loginapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.register_loginapp.db.Data;
+
+import org.litepal.LitePal;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  Integer flag=0;
+    private Button zhuce;
+    private Button denglu;
+    private EditText password;
+    private EditText user_name;
+    private CheckBox rembpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        denglu=(Button)findViewById(R.id.denglu);
+        password=(EditText) findViewById(R.id.password);
+        user_name=(EditText)findViewById(R.id.user_name);
+        rembpassword=(CheckBox)findViewById(R.id.rembpassword);
+        zhuce = findViewById(R.id.zhuce);
 
+        denglu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameinput = user_name.getText().toString().trim();
+                String pwdinput = password.getText().toString().trim();
+                List<Data> datas = LitePal.where("name = ? and mima = ?", nameinput, pwdinput).find(Data.class);
+                if (datas.size() > 0) {
+                    for (Data data : datas) {
+                        Log.d("MainActivity", "ID=" + data.getName());
+                        Log.d("MainActivity", "mima=" + data.getMima());
+                        if (data.getName().equals(nameidinput) && data.getMima().equals(pwdinput))
+                            flag = 1;
+                    }
+                }
+                if (flag == 1) {
+                    Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "登录失败，用户名或密码错误，请重新输入", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        class OnClick implements View.OnClickListener {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                switch (view.getId()) {
+                    case R.id.zhuce:
+                        intent = new Intent(MainActivity.this, register.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/register_loginapp/register.java
@@ -0,0 +1,59 @@
+package com.example.register_loginapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.register_loginapp.db.Data;
+
+import org.litepal.LitePal;
+
+import java.util.List;
+
+public class register extends AppCompatActivity {
+    String idinput,f_pwdinput,s_pwdinput;
+    private EditText number;//账号
+    private EditText f_pass;//第一次密码
+    private EditText s_pass;//第二密码
+    private Button right_zhuce;//立即注册
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+        number=(EditText)findViewById(R.id.number);
+        f_pass=(EditText)findViewById(R.id.f_pass);
+        s_pass=(EditText)findViewById(R.id.s_pass);
+        right_zhuce=(Button) findViewById(R.id.right_zhuce);
+        idinput=number.getText().toString().trim();
+        right_zhuce.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                     f_pwdinput=f_pass.getText().toString().trim();
+                    s_pwdinput=s_pass.getText().toString().trim();
+                    boolean exist=LitePal.isExist(Data.class,"name = ?",idinput);
+                    if(exist){
+                        Toast.makeText(register.this,"用户已存在，不能重复注册，请重新输入",Toast.LENGTH_SHORT).show();
+                    }else{
+                        Data newdata=new Data();
+                        newdata.setName(idinput);
+                        newdata.setMima(f_pwdinput);
+                        if(f_pwdinput==s_pwdinput) {
+                            boolean flag = newdata.save();
+                            Log.d("MainActivity", "flag=" + flag);
+                            if (flag) {
+
+                                Toast.makeText(register.this, "注册成功", Toast.LENGTH_SHORT).show();
+                            } else
+                                Toast.makeText(register.this, "注册失败", Toast.LENGTH_SHORT).show();
+                        }
+                        else Toast.makeText(register.this, "密码不一致", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
